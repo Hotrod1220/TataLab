3f43262368cb4b58e027b7863947f67f0a1c8e24
TyrellMartens <tyrell.martens@hotmail.com>
2023-11-20 11:01:51 -0700

    Final design changes.

--- src/components/common/navigation.js
+++ src/components/common/navigation.js
@@ -1,18 +1,42 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import logo from '../../data/images/TataLab_logo_black.png'
 
 function Navigation() {
     return (
         <header className="sticky-header">
             <div className="primary-nav">
-                <div className="container">
+                <div className="primary-nav__content container">
                     <Link to="/"><img src={logo} alt='TataLab Logo' /></Link>
                     <nav>
                         <ul className="primary-nav__items">
-                            <li><Link to="/research">Research</Link></li>
-                            <li><Link to="/people">People</Link></li>
-                            <li><Link to="/involved">Get Involved</Link></li>
-                            <li><Link to="/contact">Contact</Link></li>
+                            <li>
+                                <NavLink to="/research">
+                                    {({ isActive }) => (
+                                        <div className={isActive ? "nav__active" : ""}>Research</div>
+                                    )}
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/people">
+                                    {({ isActive }) => (
+                                        <div className={isActive ? "nav__active" : ""}>People</div>
+                                    )}
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/involved">
+                                    {({ isActive }) => (
+                                        <div className={isActive ? "nav__active" : ""}>Get Involved</div>
+                                    )}
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/contact">
+                                    {({ isActive }) => (
+                                        <div className={isActive ? "nav__active" : ""}>Contact</div>
+                                    )}
+                                </NavLink>
+                            </li>
                         </ul>
                     </nav>
                 </div>
